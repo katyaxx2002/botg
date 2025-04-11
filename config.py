1c6b27908d8367a14971971301971ae0d6d387d2
katyaxx2002 <katyaxx2002@mail.ru>
2025-04-11 18:03:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+token = '7561541181:AAEocnlwWq8eKFgLq4J_hd_Bt7tfTmkQ-28'
+admin = 7636773594
+
+minimalka = 1
+maximalka = 20000
+vxodadmin = 'Admin'
+vxodworker = 'Worker'
+zalety = '12345qwerttqwe241234fcdw'
+bot_username = "paradise_escortbot"
+
+poderjka = "@paradiseclub_manager"
+urlotzivi = ''
+maxpromo = 10000
+nicknameqiwi = "2204320457026252"
